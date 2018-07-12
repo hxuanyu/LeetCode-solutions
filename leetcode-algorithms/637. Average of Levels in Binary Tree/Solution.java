5b97c6c44385583ad63684e8e51c7e971d2cbd0b
Unknown <hxuanyu@gmail.com>
2018-07-12 12:08:57 +1000

    updated solution

--- leetcode-algorithms/637. Average of Levels in Binary Tree/Solution.java
+++ leetcode-algorithms/637. Average of Levels in Binary Tree/Solution.java
@@ -20,38 +20,43 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class Solution {
-    public List<Double> averageOfLevels(TreeNode root) {
+ class Solution {
+     class countandtotal {
+         public countandtotal(int c, double t) { count = c; total = t;}
+         public int count;
+         public double total;
+     }
 
-        TreeMap<Integer, Double> map_level_total = new TreeMap<Integer, Double>();
-        TreeMap<Integer, Integer> map_level_count = new TreeMap<Integer, Integer>();
-        travel(root, 0, map_level_total, map_level_count);
+     public List<Double> averageOfLevels(TreeNode root) {
 
-        ArrayList<Double> rslt = new ArrayList<Double>();
+         TreeMap<Integer, countandtotal> map = new TreeMap<Integer, countandtotal>();
+         travel(root, 0, map);
 
-        for (Map.Entry me : map_level_total.entrySet()) {
-            double total = (Double)me.getValue();
-            int level = (Integer)me.getKey();
+         ArrayList<Double> rslt = new ArrayList<Double>();
 
-            int count = map_level_count.get(me.getKey());
-            rslt.add((double)total/count);
-        }
+         for (Map.Entry me : map.entrySet()) {
+             double total = ((countandtotal)me.getValue()).total;
+             int count = ((countandtotal)me.getValue()).count;
+             rslt.add(total/count);
+         }
 
-        return rslt;
+         return rslt;
 
-    }
+     }
 
-    void travel(TreeNode node, int level, TreeMap<Integer, Double> map_level_total, TreeMap<Integer, Integer> map_level_count) {
-        if (node == null) return;
+     void travel(TreeNode node, int level, TreeMap<Integer, countandtotal> map) {
+         if (node == null) return;
 
-        if (!map_level_total.containsKey(level)) map_level_total.put(level, (double)node.val);
-        else map_level_total.put(level, map_level_total.get(level) + (double)node.val);
+         if (!map.containsKey(level)) {
+             map.put(level, new countandtotal(1, (double) node.val));
+         } else {
+             countandtotal val = map.get(level);
+             val.count++;
+             val.total += (double)node.val;
+             map.put(level, val);
+         }
 
-        if (!map_level_count.containsKey(level)) map_level_count.put(level, 1);
-        else map_level_count.put(level, map_level_count.get(level) + 1);
-
-
-        travel(node.left, level + 1, map_level_total, map_level_count);
-        travel(node.right, level + 1, map_level_total, map_level_count);
-    }
-}
+         travel(node.left, level + 1, map);
+         travel(node.right, level + 1, map);
+     }
+ }
